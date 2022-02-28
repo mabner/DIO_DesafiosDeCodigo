9e69e8aa0514b48e2fb8f65eb1c2349afc6932f3
Marcos Leite <m.abner@live.co.uk>
2022-02-28 01:54:11 -0300

    pedrobento using List intead of Array

---
+++ PedroBentoMundoOz.cs
@@ -0,0 +1,23 @@
+using System;
+
+class Program
+{
+
+	static void Main(string[] args)
+	{
+		// Utilize Console.ReadLine para leitura do input de cada linha
+		// Utilize Console.WriteLine para fazer o output de cada linha
+
+		var tesouro = new List<string>();
+		var joiasDistintas = new List<string>();
+
+		do
+		{
+			tesouro.Add(Console.ReadLine());
+		} while (!String.IsNullOrEmpty(tesouro.Last()));
+
+		joiasDistintas = tesouro.SkipLast(1).Distinct().ToList();
+
+		System.Console.WriteLine(joiasDistintas.Count);
+	}
+}
--- Program.cs
+++ Program.cs
@@ -1,36 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-// Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
 
-using System;
-
-class Program
-{
-
-	static void Main(string[] args)
-	{
-		// Utilize Console.ReadLine para leitura do input de cada linha
-		// Utilize Console.WriteLine para fazer o output de cada linha
-		string[] joiasLista;
-		do
-		{
-			joiasLista = Console.ReadLine().Split('\n');
-		} while (!Console.ReadLine().Equals(""));
-
-		Array.Sort(joiasLista);
-
-		foreach (string joia in joiasLista)
-		{
-			Console.WriteLine(joia);
-		}
-
-
-
-		// var joiasDistintas = joiasLista.Distinct();
-
-		// int countJoias = joiasDistintas.Count();
-
-		// System.Console.WriteLine(countJoias);
-	}
-}
 
 
