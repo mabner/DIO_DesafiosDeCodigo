e4c4c475c7fe8f90d0e496fb60da2508f50f5025
Marcos Leite <m.abner@live.co.uk>
2022-02-28 01:08:25 -0300

    commenting older code to be able to dotnet run

--- DividindoXPorY.cs
+++ DividindoXPorY.cs
@@ -1,24 +1,24 @@
-using System;
+// using System;
 
-class Desafio
-{
-	static void Main()
-	{
-		int limit = Int32.Parse(Console.ReadLine());
-		for (int i = 0; i < limit; i++)
-		{
-			string[] line = Console.ReadLine().Split(" ");
-			double x = double.Parse(line[0]);
-			double y = double.Parse(line[1]);
-			double divisao = x / y;
-			if (y == 0)
-			{
-				Console.WriteLine("divisao impossivel");
-			}
-			else
-			{
-				Console.WriteLine(Math.Round(divisao, 1).ToString("F1"));
-			}
-		}
-	}
-}
+// class Desafio
+// {
+// 	static void Main()
+// 	{
+// 		int limit = Int32.Parse(Console.ReadLine());
+// 		for (int i = 0; i < limit; i++)
+// 		{
+// 			string[] line = Console.ReadLine().Split(" ");
+// 			double x = double.Parse(line[0]);
+// 			double y = double.Parse(line[1]);
+// 			double divisao = x / y;
+// 			if (y == 0)
+// 			{
+// 				Console.WriteLine("divisao impossivel");
+// 			}
+// 			else
+// 			{
+// 				Console.WriteLine(Math.Round(divisao, 1).ToString("F1"));
+// 			}
+// 		}
+// 	}
+// }
--- Program.cs
+++ Program.cs
@@ -1,2 +1,25 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Console.WriteLine("Hello, World!");
+
+using System;
+
+class Program
+{
+
+	static void Main(string[] args)
+	{
+		// Utilize Console.ReadLine para leitura do input de cada linha
+		// Utilize Console.WriteLine para fazer o output de cada linha
+
+		do
+		{
+			var[] joiasLista = Console.ReadLine().Split('\n');
+		} while (!String.IsNullOrEmpty(joiasLista));
+
+		var joiasDistintas = joiasLista.Distinct();
+
+		int countJoias = joiasDistintas.Count();
+
+		System.Console.WriteLine(countJoias);
+	}
+}
