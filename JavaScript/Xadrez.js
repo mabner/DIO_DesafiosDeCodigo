967770568c387a7904d94f22e498efeaa0d5d7a4
Marcos Leite <30819867+mabner@users.noreply.github.com>
2022-03-14 17:36:17 -0300

    adds xadrez.js

---
+++ JavaScript/Xadrez.js
@@ -0,0 +1,10 @@
+let lines = gets().split("\n");
+
+var l = parseInt(lines[0]);
+var c = parseInt(lines[1]);
+
+if (l % 2 === 0)
+  if (c % 2 === 0) print(1);
+  else print(0);
+else if ((l+c) % 2 === 0) print(1);
+ else print(1);
