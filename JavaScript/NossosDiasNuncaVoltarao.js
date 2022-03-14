29622b178c6f8d3101523f3440586a33bbc1925f
Marcos Leite <30819867+mabner@users.noreply.github.com>
2022-03-14 17:40:50 -0300

    adds nossosdias nuncavoltaram.js

---
+++ JavaScript/NossosDiasNuncaVoltarao.js
@@ -0,0 +1,9 @@
+let lines = gets().split("\n");
+
+let n = parseInt(lines.shift());
+
+const citacao = "LIFE IS NOT A PROBLEM TO BE SOLVED";
+
+let resultado = citacao.substring(0,n);
+
+print (resultado);
