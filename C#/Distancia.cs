6394815f7c9b50c3b341f7fe111e6c0df75d9060
Marcos Leite <30819867+mabner@users.noreply.github.com>
2022-05-13 08:12:59 -0300

    Create Distancia.cs

---
+++ C#/Distancia.cs
@@ -0,0 +1,22 @@
+/*
+Duas motos (X e Y) partem em uma mesma direção. A moto X sai com velocidade
+constante de 60 Km/h e a moto Y sai com velocidade constante de 90 Km/h.
+
+Em uma hora (60 minutos) a moto Y consegue se distanciar 30 quilômetros da moto X, ou seja,
+consegue se afastar um quilômetro a cada 2 minutos.
+
+O seu desafio é ler a distância (em Km) e calcular quanto tempo leva (em minutos) para
+a moto Y tomar essa distância da outra m
+*/
+
+
+using System;
+
+class Desafio {
+
+    static void Main() {
+        int quilometros = Int32.Parse(Console.ReadLine());
+        int minutos = quilometros * 2;
+        Console.WriteLine(minutos + " minutos");
+    }
+}
