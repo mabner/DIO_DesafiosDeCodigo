ae6317f2d0625c55a817609b6e4b75dd57e253f7
Marcos Leite <30819867+mabner@users.noreply.github.com>
2022-05-25 13:26:36 -0300

    Create ConversaoDeTempo.cs

---
+++ C#/ConversaoDeTempo.cs
@@ -0,0 +1,12 @@
+using System;
+
+    class MinhaClasse {
+        static void Main(string[] args) {
+            var timeInSeconds = int.Parse(Console.ReadLine());
+            var hours = timeInSeconds / 3600;
+            var minutes = timeInSeconds / 60;
+            var seconds = timeInSeconds % 60;
+
+            Console.WriteLine($"{hours}:{minutes}:{seconds}");
+        }
+    }
