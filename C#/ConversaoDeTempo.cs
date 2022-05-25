5257edd629a69f83083b6046abb40586de8338e4
Marcos Leite <30819867+mabner@users.noreply.github.com>
2022-05-25 14:03:42 -0300

    Update ConversaoDeTempo.cs

--- C#/ConversaoDeTempo.cs
+++ C#/ConversaoDeTempo.cs
@@ -3,10 +3,9 @@ using System;
     class MinhaClasse {
         static void Main(string[] args) {
             var timeInSeconds = int.Parse(Console.ReadLine());
-            var hours = timeInSeconds / 3600;
-            var minutes = timeInSeconds / 60;
-            var seconds = timeInSeconds % 60;
-
+            var hours = (timeInSeconds / 3600); 
+            var minutes = (timeInSeconds % 3600) / 60; 
+            var seconds = (timeInSeconds % 3600) % 60;
             Console.WriteLine($"{hours}:{minutes}:{seconds}");
         }
     }
