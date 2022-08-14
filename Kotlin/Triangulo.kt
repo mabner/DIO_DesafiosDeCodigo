78075a9e0aff7f67c0c8ab660508b65b406973ab
Marcos Leite <m.abner@live.co.uk>
2022-08-14 14:37:11 -0300

    tringulo description update

--- Kotlin/Triangulo.kt
+++ Kotlin/Triangulo.kt
@@ -1,21 +1,22 @@
-/*
-* Desafio
-* Leia 3 valores reais (A, B e C) e verifique se eles formam ou não um triângulo. Em caso positivo, calcule o perímetro do triângulo (soma de todos os lados) e apresente a mensagem:
-*
-* Perimetro = XX.X
-*
-* Em caso negativo, calcule a área do trapézio que tem A e B como base e C como altura, mostrando a mensagem:
-*
-* Area = XX.X
-*
-* Fórmula da área de um trapézio: AREA = ((A + B) x C) / 2
-*
-* Entrada
-* A entrada contém três valores reais.
-*
-* Saída
-* O resultado deve ser apresentado com uma casa decimal.
-* */
+/**
+ * Desafio
+ * Leia 3 valores reais (A, B e C) e verifique se eles formam ou não um triângulo.
+ * Em caso positivo, calcule o perímetro do triângulo (soma de todos os lados) e apresente a mensagem:
+ *
+ * Perimetro = XX.X
+ *
+ * Em caso negativo, calcule a área do trapézio que tem A e B como base e C como altura, mostrando a mensagem:
+ *
+ * Area = XX.X
+ *
+ * Fórmula da área de um trapézio: AREA = ((A + B) x C) / 2
+ *
+ * Entrada
+ * A entrada contém três valores reais.
+ *
+ * Saída
+ * O resultado deve ser apresentado com uma casa decimal.
+ * */
 
 fun main() {
     val input = readLine()!!
