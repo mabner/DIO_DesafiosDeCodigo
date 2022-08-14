7b97f88814452a933f1af75f0e609bd7ae2ca7c7
Marcos Leite <m.abner@live.co.uk>
2022-08-14 14:18:51 -0300

    Adds Triangulo.kt

--- Java/PUM.java
+++ Java/PUM.java
@@ -1,19 +1,18 @@
 import java.io.IOException;
-import java.util.Locale;
 import java.util.Scanner;
 
-public class Main {
+class Triangulo {
 
-	public static void main(String[] args) throws IOException {
-		Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
 
-		int n, aux = 1;
+        int n, aux = 1;
 
-		n = sc.nextInt();
+        n = sc.nextInt();
 
-		for (int i = 1; i <= n; i++) {
-			System.out.printf("%d %d %d PUM\n", aux, aux + 1, aux + 2);
-			aux += 4;
-		}
-	}
+        for (int i = 1; i <= n; i++) {
+            System.out.printf("%d %d %d PUM\n", aux, aux + 1, aux + 2);
+            aux += 4;
+        }
+    }
 }
