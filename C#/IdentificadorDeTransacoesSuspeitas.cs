cdb9311f21778ee3eeb30a372af6b6f4ebfa3e56
Marcos Leite <m.abner@live.co.uk>
2025-06-28 02:26:44 -0300

    Create IdentificadorDeTransacoesSuspeitas.cs

---
+++ C#/IdentificadorDeTransacoesSuspeitas.cs
@@ -0,0 +1,49 @@
+/*
+# Identificador de Transações Suspeitas #
+
+Descrição
+Uma empresa de análise financeira está desenvolvendo um sistema para identificar transações suspeitas com base em um valor limite.
+O objetivo do sistema é verificar se alguma transação ultrapassa o valor limite e marcá-la como suspeita.
+Se houver transações suspeitas, elas devem ser listadas. Caso contrário, o sistema deve informar que não há transações suspeitas.
+Além disso, o programa deve exibir o valor total das transações suspeitas e a quantidade de transações suspeitas.
+
+Entrada
+A entrada deve conter:
+O valor limite para transações suspeitas.
+O número de transações a serem analisadas.
+Os valores das transações (valores inteiros ou decimais).
+
+Saída
+O programa deve exibir:
+O valor total das transações que ultrapassam o limite.
+O número de transações suspeitas.
+Se nenhuma transação for suspeita, o programa deve exibir "Nenhuma transação suspeita".
+
+Exemplos
+A tabela abaixo apresenta exemplos com alguns dados de entrada e suas respectivas saídas esperadas.
+Certifique-se de testar seu programa com esses exemplos e com outros casos possíveis.
+
+Entrada 	Saída
+500.00      Transacoes suspeitas: 1300.00
+5           2 transacoes suspeitas
+100.00
+200.00
+300.00
+600.00
+700.00
+
+100.00      Transacoes suspeitas: 350.00
+4           2 transacoes suspeitas
+50.00
+150.00
+200.00
+80.00
+
+250.00      Transacoes suspeitas: 300.00
+3           1 transacao suspeita
+200.00
+250.00
+300.00
+*/
+
+ 
